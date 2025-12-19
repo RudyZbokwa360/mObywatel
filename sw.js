a522620bbdd0c65c0197da6f4b354f1177847b67
RudyZbokwa360 <wiktor.bokwa1prm@gmail.com>
2025-12-19 18:35:51 +0100

    Implement asset caching in service worker
    Add caching for specified assets in service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+const cacheName = 'mobywatel-cache-v1';
+const assetsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './dashboard_files/css/main.css', // zmień na właściwe pliki CSS
+  './dashboard_files/js/main.js'    // zmień na właściwe pliki JS
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  clients.claim();
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
-
-self.addEventListener("fetch", (event) => {});
